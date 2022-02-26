edc43301dc77c0b454872637196985fbe7f317ae
Livio123123 <99071165+Livio123123@users.noreply.github.com>
2022-02-26 15:42:43 +0700

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,35 @@
+
+#===program===
+number = int(input("sesuatu-> "))
+varlist =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+for i in range (0,len(varlist)):
+    if (varlist[i]%number == 0):
+        print(i)
+
+
+
+
+
+
+
+
+#===perintah atau command===
+
+#array = ["dodi","dodi","lesr","jack","dion","jenny"]
+#print(array.count("dodi"))
+#for i in range(0,len(array)):
+#    print(array[i])
+#array.append("desi")
+#array.insert(5,"renny")
+#array.remove("dion")
+#array.pop(3)
+#array.reverse
+#print(array.count("dodi"))
+#for i in range(0,len(array)):
+#    print(array[i])
+
+
+
+
+
+
